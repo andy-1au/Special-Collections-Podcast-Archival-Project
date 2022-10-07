7af7f8e8f43bed721d4836e3d9369c8983354639
Andy <andyolau888@gmail.com>
2022-10-07 12:03:48 -0400

    testing

--- GUI App/app.py
+++ GUI App/app.py
@@ -2,39 +2,42 @@ import tkinter as tk #for GUI
 from tkinter import filedialog, Text #pick the apps 
 import os 
 import requests
+import xml.etree.ElementTree as XET
+import pandas as pd 
+import csv
 
 root = tk.Tk() # holds the entire app
 
-# xmlPath = 'C:/Users/andyr/Desktop/Special-Collections-Podcast-GUI-Project/XML'
-# csvPath = 'C:/Users/andyr/Desktop/Special-Collections-Podcast-GUI-Project/CSV'
-
-# podcastFolderPath = 'C:/Users/andyr/Desktop/Special-Collections-Podcast-GUI-Project/Podcasts'
-
-# def downloadPodcast():
-#     print("Downloading Podcast")
-#     for child in root.findall('./channel/item/'): #finds all tags in xml file under the item tag
-#         tag = child.tag
-#         if tag == 'enclosure':
-#             url = child.attrib.get('url')
-#             if url.find('/'): #finds the last '/' in the url
-#                 fileName = url.rsplit('/', 1)[1] #gets the file name from the url
-#                 filePath = os.path.join(podcastFolderPath, fileName) #creates a path for the file to be saved
-#             download = requests.get(url, allow_redirects=True) #downloads the file
-#             print(url + " has been downloaded") 
-#             open(filePath, 'wb').write(download.content) #writes the file to the path
+# Script for downloading podcasts(mp3) using rss feed(xml) tags, takes in the path to the rss feed
+def downloadPodcast(path):
+    print("Downloading Podcast")
+    tree = XET.parse(path) #parse the xml file
+    root = tree.getroot() #get the root of the xml file
+    podcastFolderPath = podcast_Folder() # Get the path to save the podcast
+    for child in root.findall('./channel/item/'): #finds all tags in xml file under the item tag
+        tag = child.tag
+        if tag == 'enclosure':
+            url = child.attrib.get('url')
+            if url.find('/'): #finds the last '/' in the url
+                fileName = url.rsplit('/', 1)[1] #gets the file name from the url
+                filePath = os.path.join(podcastFolderPath, fileName) #creates a path for the file to be saved
+            download = requests.get(url, allow_redirects=True) #downloads the file
+            print(url + " has been downloaded") 
+            open(filePath, 'wb').write(download.content) #writes the file to the path
 
 
 #asks user to select a folder to save the podcasts
 #askdirectory() only lets user select a folder
 def podcast_Folder():
-    podcastFolderPath = filedialog.askdirectory(initialdir="/", title="Select Desired Folder") 
+    podcastFolderPath = filedialog.askdirectory(initialdir="/", title="Select Where to Save the Podcasts")
     pdPathLabel = tk.Label(root, text="Save Podcasts Path: " + podcastFolderPath) # Create a label to display the path of the podcast folder
     pdPathLabel.pack()
     print(podcastFolderPath)
+    return podcastFolderPath
 
 #asks user to select the RSS xml file and save the path 
 def select_RSS_Feed():
-    xmlPath = filedialog.askopenfilename(initialdir="/", title="Select Desired RSS", filetypes=(("xml files", "*.xml"), ("all files", "*.*")))
+    xmlPath = filedialog.askopenfilename(initialdir="/", title="Select Your RSS File", filetypes=(("xml files", "*.xml"), ("all files", "*.*")))
     # Create a label to display the path of the rss feed
     xmlPathLabel = tk.Label(root, text="RSS Feed Path: " + xmlPath)
     xmlPathLabel.pack()
@@ -42,7 +45,7 @@ def select_RSS_Feed():
 
 #asks user to select the folder to save the downloaded rss feed
 def save_RSS_Feed():
-    xmlPath = filedialog.askdirectory(initialdir="/", title="Select Desired Folder")
+    xmlPath = filedialog.askdirectory(initialdir="/", title="Select Where to Save the RSS Feed")
     xmlPathLabel = tk.Label(root, text="Save RSS Feed Path: " + xmlPath)
     xmlPathLabel.pack() # Create a label to display the path of the rss feed
     print(xmlPath)
@@ -53,17 +56,18 @@ def removePlaceholder(event):
     return None
 
 def getRSSEntry(): 
-    # Get the RSS feed from the entry box
-    rssLink = enterRSS.get()
+    rssLink = enterRSS.get() # Get the RSS feed from the entry box
     print(rssLink) # DEBUG
-    # use the rssLink to download the podcast
-    xmlPath = save_RSS_Feed()
-    downloadPodcast(rssLink, xmlPath)
+    xmlPath = save_RSS_Feed() # Get the path to save the RSS feed, requires user input
+    rssPath = downloadRSS(rssLink, xmlPath) # Get the path to the downloaded RSS feed
+    downloadPodcast(rssPath) # Use the path of RSS feed for download podcast function
 
-def downloadPodcast(entry, xmlPath):
+def downloadRSS(entry, xmlPath):
     response = requests.get(entry, allow_redirects=True)
-    open(xmlPath+"/rssFeed.xml", 'wb').write(response.content) #writes the content of the rss feed to a specified file named podcast.xml
-    print("Podcast Downloaded")
+    rssPath = os.path.join(xmlPath, "rss.xml")
+    open(rssPath, 'wb').write(response.content) #writes the content of the rss feed to a specified file named podcast.xml
+    print("RSS Feed has been downloaded")
+    return rssPath # Return the path to the rss feed
 
 #attach the canvas to the root
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42") 
