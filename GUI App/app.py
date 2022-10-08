973128562ad75fc63283e0181eb1adf426235c7c
Andy <andyolau888@gmail.com>
2022-10-08 00:32:47 -0400

    worked on some styling

--- GUI App/app.py
+++ GUI App/app.py
@@ -1,13 +1,12 @@
 import tkinter as tk #for GUI
-from tkinter import CENTER, filedialog, Text #pick the apps 
+from tkinter import filedialog, Text, CENTER
+from tkinter import ttk
 import os
 from traceback import print_tb 
 import requests
 import xml.etree.ElementTree as XET
 import pandas as pd 
 import csv
-
-root = tk.Tk() # holds the entire app
 
 # Script for downloading podcasts(mp3) using rss feed(xml) tags, takes in the path to the rss feed
 def download_PD(path):
@@ -54,10 +53,12 @@ def save_RSS_Feed():
     print(xmlPath)
     return xmlPath
 
+# remove the default text from the entry box
 def remove_PH(event):
     enterRSS.delete(0, "end")
     return None
 
+# function to save the entered rss feed url to a string
 def get_RSS_Entry(): 
     rssLink = enterRSS.get() # Get the RSS feed from the entry box
     print(rssLink) # DEBUG
@@ -65,12 +66,13 @@ def get_RSS_Entry():
     rssPath = downloadRSS(rssLink, xmlPath) # Get the path to the downloaded RSS feed
     download_PD(rssPath) # Use the path of RSS feed for download podcast function
 
+# function to download the rss feed from the url and return the path to the downloaded rss feed
 def downloadRSS(entry, xmlPath):
     response = requests.get(entry, allow_redirects=True)
     rssPath = os.path.join(xmlPath, "rss.xml")
     open(rssPath, 'wb').write(response.content) #writes the content of the rss feed to a specified file named podcast.xml
     printToGUI("RSS Feed has been downloaded\n")
-    return rssPath # Return the path to the rss feed
+    return rssPath 
 
 # make a function to print terminal output to the GUI textbox
 def printToGUI(text):
@@ -78,11 +80,29 @@ def printToGUI(text):
     outputBox.insert("end", text + "")
     root.update()
 
-#attach the canvas to the root
-canvas = tk.Canvas(root, height=700, width=700, bg="#263D42").pack() #pack the canvas to the root so it can be seen
+root = tk.Tk() # holds the entire app
+root.title("Podcast Downloader")
 
-frame = tk.Frame(root, bg="white") #create a frame
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1) #place the frame in the middle of the canvas
+#------------------Working On Styling------------------
+# #Add some styling to the app
+# style = ttk.Style()
+# style.configure("Treeview",
+#     background = "silver",
+#     foreground = "black",
+#     rowheight = 25,
+#     fieldbackground = "silver")
+
+# style.map('Treeview',
+#     background = [('selected', 'blue')]) 
+
+# my_tree = ttk.Treeview(root)
+#------------------Working On Styling------------------
+
+#attach the canvas to the root
+canvas = tk.Canvas(root, height=1000, width=1000, bg="white").pack() #pack the canvas to the root so it can be seen
+
+frame = tk.Frame(root, bg="black") #create a frame
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1) #place the frame in the root
 
 # --------------  Buttons Section ----------------- 
 #Select RSS Feed from File Button
@@ -97,7 +117,8 @@ tk.Button(frame, text="Quit", padx=10, pady=5, fg="white", bg="#263D42", command
 
 # ----------------- Entry Section -----------------
 # make an entry box for rss feed, add a float label in the entry box
-enterRSS = tk.Entry(frame, width=50, borderwidth=5)
+enterRSS = tk.Entry(frame, width=50, borderwidth=5, bg="#263D42", fg="white")
+enterRSS.configure(justify=CENTER)
 enterRSS.insert("end", "Enter RSS Feed Here")
 enterRSS.bind("<Button-1>", remove_PH)
 enterRSS.pack(side="top", fill="x", expand=True)
@@ -105,7 +126,7 @@ enterRSS.pack(side="top", fill="x", expand=True)
 
 # ----------------- Text Box Section -----------------
 # make a text box for terminal output
-outputBox = tk.Text(frame, height=50, width=50, borderwidth=5)
+outputBox = tk.Text(frame, height=50, width=50, borderwidth=5, bg="#263D42")
 outputBox.pack()
 # ----------------- Text Box Section -----------------
 
