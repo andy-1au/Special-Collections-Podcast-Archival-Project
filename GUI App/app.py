76ba1fae661045e517691fc78cee1b70a1bdd84b
Andy <andyolau888@gmail.com>
2022-10-07 22:09:29 -0400

    added a textbox for commandline outputs, renamed functions, added quit button

--- GUI App/app.py
+++ GUI App/app.py
@@ -1,18 +1,17 @@
 import tkinter as tk #for GUI
 from tkinter import filedialog, Text #pick the apps 
-import os 
+import os
+from traceback import print_tb 
 import requests
 import xml.etree.ElementTree as XET
 import pandas as pd 
 import csv
 
-#test working tree
-
 root = tk.Tk() # holds the entire app
 
 # Script for downloading podcasts(mp3) using rss feed(xml) tags, takes in the path to the rss feed
-def downloadPodcast(path):
-    print("Downloading Podcast")
+def download_PD(path):
+    printToGUI("Downloading Podcast\n")
     tree = XET.parse(path) #parse the xml file
     root = tree.getroot() #get the root of the xml file
     podcastFolderPath = podcast_Folder() # Get the path to save the podcast
@@ -24,11 +23,11 @@ def downloadPodcast(path):
                 fileName = url.rsplit('/', 1)[1] #gets the file name from the url
                 filePath = os.path.join(podcastFolderPath, fileName) #creates a path for the file to be saved
             download = requests.get(url, allow_redirects=True) #downloads the file
-            print(url + " has been downloaded") 
+            printToGUI(url + " has been downloaded\n") 
             open(filePath, 'wb').write(download.content) #writes the file to the path
-    print("All podcasts has been downloaded") 
-    download.close() #closes the connection #DEBUG
-
+    printToGUI("All podcasts has been downloaded\n") 
+    download.close() #closes the connection to the server
+    printToGUI("Connection Closed\n") #DEBUG, even when the request is closed, the app window still runs
 
 #asks user to select a folder to save the podcasts
 #askdirectory() only lets user select a folder
@@ -55,46 +54,60 @@ def save_RSS_Feed():
     print(xmlPath)
     return xmlPath
 
-def removePlaceholder(event):
+def remove_PH(event):
     enterRSS.delete(0, "end")
     return None
 
-def getRSSEntry(): 
+def get_RSS_Entry(): 
     rssLink = enterRSS.get() # Get the RSS feed from the entry box
     print(rssLink) # DEBUG
     xmlPath = save_RSS_Feed() # Get the path to save the RSS feed, requires user input
     rssPath = downloadRSS(rssLink, xmlPath) # Get the path to the downloaded RSS feed
-    downloadPodcast(rssPath) # Use the path of RSS feed for download podcast function
+    download_PD(rssPath) # Use the path of RSS feed for download podcast function
 
 def downloadRSS(entry, xmlPath):
     response = requests.get(entry, allow_redirects=True)
     rssPath = os.path.join(xmlPath, "rss.xml")
     open(rssPath, 'wb').write(response.content) #writes the content of the rss feed to a specified file named podcast.xml
-    print("RSS Feed has been downloaded")
+    printToGUI("RSS Feed has been downloaded\n")
     return rssPath # Return the path to the rss feed
 
+# make a function to print terminal output to the GUI textbox
+def printToGUI(text):
+    print(text)
+    outputBox.insert("end", text + "")
+    root.update()
+
 #attach the canvas to the root
-canvas = tk.Canvas(root, height=700, width=700, bg="#263D42") 
-canvas.pack() #pack the canvas to the root so it can be seen
+canvas = tk.Canvas(root, height=700, width=700, bg="#263D42").pack() #pack the canvas to the root so it can be seen
 
 frame = tk.Frame(root, bg="white") #create a frame
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1) #place the frame in the middle of the canvas
 
-# button for selecting the rss feed
-chooseRSS = tk.Button(frame, text="Select RSS Feed", padx=10, pady=5, fg="white", bg="#263D42", command=select_RSS_Feed) 
-chooseRSS.pack() 
+# --------------  Buttons Section ----------------- 
+# Select RSS Feed from File Button
+tk.Button(frame, text="Select RSS Feed", padx=10, pady=5, fg="white", bg="#263D42", command=select_RSS_Feed).pack() 
 
-# button for selecting the folder to save the podcasts
-choosePodcastFolder = tk.Button(frame, text="Select Podcasts Folder", padx=10, pady=5, fg="white", bg="#263D42", command=podcast_Folder) 
-choosePodcastFolder.pack() 
+# Submit RSS Feed button
+tk.Button(frame, text="Submit RSS Feed", padx=10, pady=5, fg="white", bg="#263D42", command=get_RSS_Entry).pack()
 
-submitRSS = tk.Button(frame, text="Submit RSS Feed", padx=10, pady=5, fg="white", bg="#263D42", command=getRSSEntry)
-submitRSS.pack()
+# Quit button
+tk.Button(root, text="Quit", padx=10, pady=5, fg="white", bg="#263D42", command=root.destroy).pack()
+# --------------  Buttons Section ----------------- 
 
+# ----------------- Entry Section -----------------
 # make an entry box for rss feed, add a float label in the entry box
 enterRSS = tk.Entry(frame, width=50, borderwidth=5)
 enterRSS.insert(0, "Enter RSS Feed")
-enterRSS.bind("<Button-1>", removePlaceholder)
+enterRSS.bind("<Button-1>", remove_PH)
 enterRSS.pack()
+# ----------------- Entry Section -----------------
+
+# ----------------- Text Box Section -----------------
+# make a text box for terminal output
+outputBox = tk.Text(frame, height=50, width=50, borderwidth=5)
+outputBox.pack()
+# ----------------- Text Box Section -----------------
 
 root.mainloop() #similar to html, this is what keeps the window open
+
