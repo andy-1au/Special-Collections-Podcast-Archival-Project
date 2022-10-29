07cb35636161c76351ab6cf49c304ddb8680f13c
Andy <andyolau888@gmail.com>
2022-10-29 00:11:22 -0400

    added and tested convert_to_csv(, working on download_PD()

--- GUI App/app2.py
+++ GUI App/app2.py
@@ -2,22 +2,36 @@ from pathlib import Path
 
 import PySimpleGUI as sg 
 import pandas as pd
+import requests
+import os 
 
+#------------------RSS LINK------------------#
+#https://feeds.captivate.fm/gogetters/
+#------------------RSS LINK------------------#
 
 
 # def convert_to_csv():
+def download_RSS(url, rssDest):
+    response = requests.get(url, allow_redirects=True)
+    rssPath = os.path.join(rssDest, "rss.xml") #creates a path for the file to be saved
+    open(rssPath, 'wb').write(response.content) #writes the content of the rss feed to a specified file named podcast.xml
+    print("RSS Feed has been downloaded")
+    return rssPath #not sure if this is needed
 
+# def download_PD(xml, podcastDest): #look at app.py for reference 
+    
 
 
 #Layout
 layout = [
         #each line in the layout list is a row 
         #LATER - add placeholder and disappear when clicked
-        [sg.Text("RSS Feed Link:"), sg.Input(key="-IN2-")],
-        [sg.Text("XML File:"), sg.Input(key="-IN-"), sg.FileBrowse(file_types=(("XML Files", "*.xml"),))], 
-        [sg.Text("Podcast Destination Folder:"), sg.Input(key="-POD-"), sg.FolderBrowse()],
-        [sg.Text("CSV Destination Folder:"), sg.Input(key="-CSV-"), sg.FolderBrowse()],
-        [sg.Exit(), sg.Button("Download Podcasts"), sg.Button("Convert To CSV")]    
+        [sg.Text("RSS Feed Link:"), sg.Input(key="-RSS_URL-")],
+        [sg.Text("XML File:"), sg.Input(key="-XML_File-"), sg.FileBrowse(file_types=(("XML Files", "*.xml"),))], 
+        [sg.Text("RSS Destination:"), sg.Input(key="-RSS_DEST-"), sg.FolderBrowse()],
+        [sg.Text("Podcast Destination:"), sg.Input(key="-POD_DEST-"), sg.FolderBrowse()],
+        [sg.Text("CSV Destination:"), sg.Input(key="-CSV_DEST-"), sg.FolderBrowse()],
+        [sg.Exit(), sg.Button("Download RSS"), sg.Button("Download Podcasts"), sg.Button("Convert To CSV")]    
 ]
 
 window = sg.Window("Podcast Downloader", layout)
@@ -27,9 +41,13 @@ while True:
     print(event, values) #DEBUG
     if event == sg.WIN_CLOSED or event == "Exit": #WIN_CLOSED = X button, Exit = Exit button
         break #break out of the loop and close the window
-    if event == "Download Podcasts":
-        print("Download Podcasts") #DEBUG
+    if event == "Download RSS":
+        print("Downloading RSS Feed") #DEBUG
+        download_RSS(values["-RSS_URL-"], values["-RSS_DEST-"])
         
+        
+    if event == "Download Podcasts":
+        print("Downloading Podcasts") #DEBUG 
     if event == "Convert To CSV":
-        print("Convert To CSV") #DEBUG
+        print("Converting To CSV") #DEBUG
 window.close() 
